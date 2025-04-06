96b133e5f9dc143dcb2e35ee4661eee40a4286ae
Eberhard Wolff <eberhard.wolff@posteo.net>
2025-04-06 23:54:39 +0200

    Remove unneeded imports

--- microservice-spring-demo/microservice-spring-order/src/main/java/com/ewolff/microservice/order/logic/OrderRestController.java
+++ microservice-spring-demo/microservice-spring-order/src/main/java/com/ewolff/microservice/order/logic/OrderRestController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
